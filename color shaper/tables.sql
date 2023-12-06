e38d9f05c991ca2c6746e45af67403491cae4236
Avile Sindelo <avilesindelo@gmail.com>
2023-12-06 12:59:45 +0200

    creating the other tables - player and player_stats

--- color shaper/tables.sql
+++ color shaper/tables.sql
@@ -4,3 +4,17 @@ CREATE TABLE questions (
    game_level text not null
 );
 
+CREATE TABLE player (
+   id serial primary key,
+   name text not null,
+   unique_code varchar(255);
+);
+
+CREATE TABLE player_stats (
+   id serial primary key,
+   score int not null,
+   level text not null,
+   hint_count int not null,
+   foreign key (player_id) references player(id)
+)
+
